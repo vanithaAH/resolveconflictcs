7b72cdad3fa995fe3479998a423f92582e6a69ac
vanithaAH <vanithash123@gmail.com>
2022-07-20 18:58:39 +0530

    dev2commit

--- HelloCode.cs
+++ HelloCode.cs
@@ -7,7 +7,7 @@ namespace CSharpSolutions
          static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World dev2");
 
         }
     }
